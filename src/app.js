76097a6718a6c562fec74e5bab228ef2b4e471dd
RemiBertrand <rbertrand21@gmail.com>
2023-12-11 16:06:54 +0100

    Setup

--- src/app.js
+++ src/app.js
@@ -1,19 +1,16 @@
-import { Routes, Route } from 'react-router-dom'
-import './style/main.css'
-import Home from './pages/Home'
-import Profile from './pages/Profile'
-import Login from './pages/Login'
-import Error from './pages/Error'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Login from './pages/Login';
+import Error from './pages/Error';
 
 export default function App() {
-	return (
-		<>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/profile/" element={<Profile />} />
-				<Route path="*" element={<Error />} />
-			</Routes>
-		</>
-	)
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile/" element={<Profile />} />
+            <Route path="/*" element={<Error />} />
+        </Routes>
+    );
 }
